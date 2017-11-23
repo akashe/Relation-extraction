568502859595d921bf62ab4a669f5bdfc638963c
Akash Kumar <akash.kumar2@ipsoft.com>
2017-11-24 00:31:55 +0530

    updating run file

--- network.py
+++ network.py
@@ -14,76 +14,79 @@ class network_settings():
     sen_split_len = 10
     number_of_layers = 3
     embedding_size = 50
-    num_classes = 40
+    num_classes = 15
     num_filter = 10
     first_layer_units = 40
     second_layer_units = 40
     third_layer_units = 40
-
+    batch_size = 100
+    epochs = 10
+    test_batch_size = 1000
 
 class model():
-    settings = network_settings()
-    input_x = tf.placeholder(dtype=tf.int32, shape=[None, settings.max_sen_len], name="input_x")
-    input_y = tf.placeholder(dtype=tf.int32, shape=[None, settings.num_classes], name="input_y")
-
-    pooled_outputs =[]
-
-    embedded_input = tf.expand_dims(
-        tf.nn.embedding_lookup(tf.get_variable(initializer=(np.load("data/embeddings.npy").astype(dtype='float32')), name="embeddings"),
-                               input_x), -1)
-    with tf.name_scope("entire_sentence"):
-        conv = tf.nn.conv2d(embedded_input, tf.Variable(tf.truncated_normal([settings.max_sen_len, settings.embedding_size, 1, settings.num_filter], stddev=0.1,dtype=tf.float32)),
-                            strides=[1, 1, 1, 1], padding="VALID", name="conv")
-        h = tf.nn.relu(tf.nn.bias_add(conv,tf.Variable(tf.constant(0.1,shape = [settings.num_filter])),name = "relu"))
-
-        pooled = tf.nn.max_pool(h,ksize=[1,1,1,settings.num_filter],strides=[1,1,1,1],padding='VALID',name="pool")
-        pooled_outputs.append(pooled)
-
-    with tf.name_scope("parts_of_sentence"):
-        # part_length = settings.max_sen_len/settings.sen_split_len
-        # for i in range(0,part_length):
-        #     with tf.name_scope("sentence_part : "+str(i)):
-        conv1 = tf.nn.conv2d(embedded_input, tf.Variable(
-                    tf.truncated_normal([settings.sen_split_len, settings.embedding_size, 1, settings.num_filter],
-                                        stddev=0.1, dtype=tf.float32)),
-                                    strides=[1, settings.sen_split_len, 1, 1], padding="VALID", name="sentence_part_conv")
-        h1 = tf.nn.relu(tf.nn.bias_add(conv1, tf.Variable(tf.constant(0.1, shape=[settings.num_filter])), name="sentence_part_relu"))
-        pooled1 = tf.nn.max_pool(h1, ksize=[1, 1, 1, settings.num_filter], strides=[1, 1, 1, 1], padding='VALID',
-                                name="sentence_part_pool")
-        pooled_outputs.append(pooled1)
-
-    pooled_output = tf.squeeze(tf.concat(concat_dim=1,values=pooled_outputs),squeeze_dims=[-2,-1])
-
-    with tf.name_scope("Fully_connected_nueral_network"):
-        with tf.name_scope("First-layer"):
-            W_first = tf.Variable(tf.truncated_normal([int(pooled_output._shape[-1]),settings.first_layer_units],mean=0.0,stddev=1.0),name = "W_First")
-            b_first = tf.Variable(tf.constant(0.1,shape=[settings.first_layer_units]))
-            hidden_layer_1 = tf.nn.relu(tf.matmul(pooled_output,W_first)+b_first,name="hidden_layer_1_output")
-
-        with tf.name_scope("Second-layer"):
-            W_second = tf.Variable(tf.truncated_normal([settings.first_layer_units,settings.second_layer_units],mean=0.0,stddev=1.0),name="W_Second")
-            b_second = tf.Variable(tf.constant(-0.1,shape=[settings.second_layer_units]))
-            hidden_layer_2 = tf.nn.dropout(tf.nn.relu(tf.matmul(hidden_layer_1,W_second)+b_second,name="hidden_layer_2_output"),keep_prob=0.1)
-
-        with tf.name_scope("Third-layer"):
-            W_third = tf.Variable(tf.truncated_normal([settings.second_layer_units,settings.third_layer_units],mean=0.0,stddev=1.0),name="W_Third")
-            b_third = tf.Variable(tf.constant(-0.1, shape=[settings.third_layer_units]))
-            hidden_layer_3 = tf.nn.dropout(tf.nn.relu(tf.matmul(hidden_layer_1, W_third) + b_third, name="hidden_layer_3_output"),keep_prob=0.1)
-
-    with tf.name_scope("output"):
-        W_output = tf.get_variable("W_output",shape=[settings.third_layer_units,settings.num_classes],initializer=tf.contrib.layers.xavier_initializer())
-        b_output = tf.Variable(tf.constant(0.1,shape=[settings.num_classes]),name="b_output")
-        score = tf.nn.xw_plus_b(hidden_layer_3,W_output,b_output,name="score")
-
-    with tf.name_scope("loss"):
-        loss = tf.nn.softmax_cross_entropy_with_logits(labels=input_y,logits=score)
-        l2_loss = tf.contrib.layers.apply_regularization(regularizer=tf.contrib.layers.l2_regularizer(0.0001),
-                                                              weights_list=tf.trainable_variables())
-        total_loss = loss +l2_loss
-        tf.scalar_summary("total_loss",total_loss)
-
-    with tf.name_scope("accuracy"):
-        accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(score,1),tf.argmax(input_y,1)),"float"),name="accuracy")
-        tf.scalar_summary("accuracy",accuracy)
-
-
+    def __init__(self):
+        self.settings = network_settings()
+        self.input_x = tf.placeholder(dtype=tf.int32, shape=[None, self.settings.max_sen_len], name="input_x")
+        self.input_y = tf.placeholder(dtype=tf.int32, shape=[None, self.settings.num_classes], name="input_y")
+    
+        pooled_outputs =[]
+    
+        embedded_input = tf.expand_dims(
+            tf.nn.embedding_lookup(tf.get_variable(initializer=(np.load("data/embeddings.npy").astype(dtype='float32')), name="embeddings"),
+                                   self.input_x), -1)
+        with tf.name_scope("entire_sentence"):
+            conv = tf.nn.conv2d(embedded_input, tf.Variable(tf.truncated_normal([self.settings.max_sen_len, self.settings.embedding_size, 1, self.settings.num_filter], stddev=0.1,dtype=tf.float32)),
+                                strides=[1, 1, 1, 1], padding="VALID", name="conv")
+            h = tf.nn.relu(tf.nn.bias_add(conv,tf.Variable(tf.constant(0.1,shape = [self.settings.num_filter])),name = "relu"))
+    
+            pooled = tf.nn.max_pool(h,ksize=[1,1,1,self.settings.num_filter],strides=[1,1,1,1],padding='VALID',name="pool")
+            pooled_outputs.append(pooled)
+    
+        with tf.name_scope("parts_of_sentence"):
+            # part_length = self.settings.max_sen_len/self.settings.sen_split_len
+            # for i in range(0,part_length):
+            #     with tf.name_scope("sentence_part : "+str(i)):
+            conv1 = tf.nn.conv2d(embedded_input, tf.Variable(
+                        tf.truncated_normal([self.settings.sen_split_len, self.settings.embedding_size, 1, self.settings.num_filter],
+                                            stddev=0.1, dtype=tf.float32)),
+                                        strides=[1, self.settings.sen_split_len, 1, 1], padding="VALID", name="sentence_part_conv")
+            h1 = tf.nn.relu(tf.nn.bias_add(conv1, tf.Variable(tf.constant(0.1, shape=[self.settings.num_filter])), name="sentence_part_relu"))
+            pooled1 = tf.nn.max_pool(h1, ksize=[1, 1, 1, self.settings.num_filter], strides=[1, 1, 1, 1], padding='VALID',
+                                    name="sentence_part_pool")
+            pooled_outputs.append(pooled1)
+    
+        pooled_output = tf.squeeze(tf.concat(concat_dim=1,values=pooled_outputs),squeeze_dims=[-2,-1])
+    
+        with tf.name_scope("Fully_connected_nueral_network"):
+            with tf.name_scope("First-layer"):
+                W_first = tf.Variable(tf.truncated_normal([int(pooled_output._shape[-1]),self.settings.first_layer_units],mean=0.0,stddev=1.0),name = "W_First")
+                b_first = tf.Variable(tf.constant(0.1,shape=[self.settings.first_layer_units]))
+                hidden_layer_1 = tf.nn.relu(tf.matmul(pooled_output,W_first)+b_first,name="hidden_layer_1_output")
+    
+            with tf.name_scope("Second-layer"):
+                W_second = tf.Variable(tf.truncated_normal([self.settings.first_layer_units,self.settings.second_layer_units],mean=0.0,stddev=1.0),name="W_Second")
+                b_second = tf.Variable(tf.constant(-0.1,shape=[self.settings.second_layer_units]))
+                hidden_layer_2 = tf.nn.dropout(tf.nn.relu(tf.matmul(hidden_layer_1,W_second)+b_second,name="hidden_layer_2_output"),keep_prob=0.1)
+    
+            with tf.name_scope("Third-layer"):
+                W_third = tf.Variable(tf.truncated_normal([self.settings.second_layer_units,self.settings.third_layer_units],mean=0.0,stddev=1.0),name="W_Third")
+                b_third = tf.Variable(tf.constant(-0.1, shape=[self.settings.third_layer_units]))
+                hidden_layer_3 = tf.nn.dropout(tf.nn.relu(tf.matmul(hidden_layer_2, W_third) + b_third, name="hidden_layer_3_output"),keep_prob=0.1)
+    
+        with tf.name_scope("output"):
+            W_output = tf.get_variable("W_output",shape=[self.settings.third_layer_units,self.settings.num_classes],initializer=tf.contrib.layers.xavier_initializer())
+            b_output = tf.Variable(tf.constant(0.1,shape=[self.settings.num_classes]),name="b_output")
+            score = tf.nn.xw_plus_b(hidden_layer_3,W_output,b_output,name="score")
+    
+        with tf.name_scope("loss"):
+            self.loss = tf.nn.softmax_cross_entropy_with_logits(labels=self.input_y,logits=score)
+            self.l2_loss = tf.contrib.layers.apply_regularization(regularizer=tf.contrib.layers.l2_regularizer(0.0001),
+                                                                  weights_list=tf.trainable_variables())
+            self.total_loss = self.loss +self.l2_loss
+            tf.scalar_summary("total_loss",self.total_loss)
+    
+        with tf.name_scope("accuracy"):
+            self.accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(score,1),tf.argmax(self.input_y,1)),"float"),name="accuracy")
+            tf.scalar_summary("accuracy",self.accuracy)
+    
+    
--- run.py
+++ run.py
@@ -1,9 +1,15 @@
 import gzip
 import numpy as np
 import network
-import data_extractor
 import tensorflow as tf
 import operator
+import pickle
+import datetime
+
+# TODO create data from word word to number
+# TODO fix sentence len to max len
+
+
 
 def get_word_embeddings():
     word_to_number = {}
@@ -22,7 +28,7 @@ def get_word_embeddings():
     return word_to_number
 
 def preen_data():
-    #reduce number of classes to 16
+    #reduce number of classes to 15 (excluding NA also)
     reduced_count = 16
     relation_to_count ={}
     with gzip.open("data/test.txt.gz","rb") as f:
@@ -48,10 +54,12 @@ def preen_data():
                 relation_to_count[relation]+=1
 
     top_relations = {}
-    for k in sorted(relation_to_count.items(),key= operator.itemgetter(1),reverse=True)[:reduced_count]:
-        top_relations[k[0]]=1
-        
-    with gzip.open("data/preenedtest.txt.gz","wb") as f_write:
+    relation_count = 0
+    for k in sorted(relation_to_count.items(),key= operator.itemgetter(1),reverse=True)[1:reduced_count]:
+        top_relations[k[0]]=relation_count
+        relation_count+=1
+
+    with gzip.open("data/preened_test.txt.gz","wb") as f_write:
         with gzip.open("data/test.txt.gz","rb") as f:
             for line in f:
                 try:
@@ -61,7 +69,7 @@ def preen_data():
                 if relation in top_relations:
                     f_write.write(line)
 
-    with gzip.open("data/preenedtrain.txt.gz","wb") as f_write:
+    with gzip.open("data/preened_train.txt.gz","wb") as f_write:
         with gzip.open("data/test.txt.gz","rb") as f:
             for line in f:
                 try:
@@ -70,22 +78,145 @@ def preen_data():
                     continue
                 if relation in top_relations:
                     f_write.write(line)
+    with open("data/relations","wb") as f:
+        pickle.dump(obj=top_relations,file=f)
 
-def get_train_data():
-    pass
 
-def get_test_data():
-    pass
+def get_train_data(num_of_classes):
+    train_x=[]
+    train_y=[]
+    relations = pickle.load(open("data/relations","rb"))
+    with gzip.open("data/preened_train.txt.gz","rb") as f:
+        for line in f:
+            line = line.decode("ascii").strip().split("\t")
+            relation = line[4]
+            sentence = line[5]
+            if relation in relations:
+                train_x.append(sentence)
+                label = [ 0 for i in range(num_of_classes)]
+                label[relations[relation]] = 1
+                train_y.append(label)
+    np.save("data/train_x.npy",train_x)
+    np.save("data/train_y.npy",train_y)
+
+def get_test_data(num_of_classes):
+    test_x = []
+    test_y = []
+    relations = pickle.load(open("data/relations","rb"))
+    with gzip.open("data/preened_test.txt.gz", "rb") as f:
+        for line in f:
+            line = line.decode("ascii").strip().split("\t")
+            relation = line[4]
+            sentence = line[5]
+            if relation in relations:
+                test_x.append(sentence)
+                label = [0 for i in range(num_of_classes)]
+                label[relations[relation]] = 1
+                test_y.append(label)
+    np.save("data/test_x.npy", test_x)
+    np.save("data/test_y.npy", test_y)
 
 def get_model():
     model = network.model()
     return model
 
 def train():
-    word_embeddings = get_word_embeddings()
+    # word_embeddings = get_word_embeddings()
+    # settings = network.network_settings()
+    # get_train_data(settings.num_classes)
+    # get_test_data(settings.num_classes)
+
+    save_path = "./model/"
+
+    print("Reading train and test data....")
+
+    train_x = np.load("data/train_x.npy")
+    train_y = np.load("data/train_y.npy")
+    test_x = np.load("data/test_x.npy")
+    test_y = np.load("data/test_y.npy")
+
+    assert len(train_x)==len(train_y)
+    assert len(test_x)==len(test_y)
+
+    with tf.Graph().as_default():
+
+        sess = tf.Session()
+        with sess.as_default():
+
+            intializer = tf.contrib.layers.xavier_initializer()
+            with tf.variable_scope("model",initializer=intializer):
+                model = network.model()
+            global_step = tf.Variable(0,name="global_step",trainable=False)
+            optimizer = tf.train.AdamOptimizer(0.001)
+
+            train_op = optimizer.minimize(model.total_loss,global_step=global_step)
+            sess.run(tf.initialize_all_variables())
+            saver = tf.train.Saver()
+
+            merged_summary = tf.merge_all_summaries()
+            summary_w = tf.train.SummaryWriter("summary",sess.graph)
+
+            batch_size = model.settings.batch_size
+            test_batch_size = model.settings.test_batch_size
+            num_classs = model.settings.num_classes
+            epochs = model.settings.epochs
+
+            for i in range(epochs):
+
+                order = range(len(train_x))
+                np.random.shuffle(order)
+
+                for j in range(len(train_x)/batch_size):
+                    feed_dict ={}
+                    temp_x =[]
+                    temp_y= []
+                    for l in order[batch_size*j:min(batch_size*(j+1),len(train_x))]:
+                        temp_x.append(train_x[l])
+                        temp_y.append(train_y[l])
+
+                    feed_dict[model.input_x]=temp_x
+                    feed_dict[model.input_y]=temp_y
+
+                    _,global_step,loss,accuracy,summary =sess.run([train_op,global_step,model.total_loss,model.accuracy,merged_summary],feed_dict)
+                    summary_w.add_summary(accuracy,global_step)
+
+                    if global_step % 20 == 0:
+                        time_str = datetime.datetime.now().isoformat()
+                        print ("{}: step {} , loss {:g}, acc {:g}".format(time_str,global_step,loss,accuracy))
+
+                order_test = range(len(test_x))
+                np.random.shuffle(order_test)
+                acc =[]
+                for j in range(len(test_x) / test_batch_size):
+                    feed_dict = {}
+                    temp_x =[]
+                    temp_y =[]
+                    for l in order[test_batch_size*j:min(test_batch_size*(j+1),len(test_y))]:
+                        temp_x.append(test_x[l])
+                        temp_y.append(test_y[l])
+
+                    feed_dict[model.input_x] = temp_x
+                    feed_dict[model.input_y] = temp_y
+
+                    accuracy = sess.run(
+                        [ model.accuracy], feed_dict)
+                    acc.append(accuracy)
+
+                print("Epoch: {}, test_Accuracy: {} ".format(i,np.mean(acc)))
 
 
 
-# train()
+
+
+
+
+
+
+
+
+
+
+train()
 # get_model()
-preen_data()
+#preen_data()
+
